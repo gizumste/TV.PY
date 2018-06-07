f8d4c8af69c87e72547662c7f918acf316d72e02
gizumste <39944243+gizumste@users.noreply.github.com>
2018-06-07 10:21:14 +0200

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,9 +1,7 @@
-
 #!/bin/bash
 sleep 10s
-firefox-esr http://loxcom:mocxol@ene02-cam01.lextend.ch:20011/control/faststream.jpg?stream=$
+firefox-esr http://loxcom:mocxol@ene02-cam01.lextend.ch:20011/control/faststream.jpg?stream=full&FPS=2 &
 sleep 10s
-firefox-esr http://loxcom:mocxol@ene02-cam02.lextend.ch:20012/control/faststream.jpg?stream=$
+firefox-esr http://loxcom:mocxol@ene02-cam02.lextend.ch:20012/control/faststream.jpg?stream=full&fps=2 &
 sleep 10s
 firefox-esr /home/pi/Documents/monitoring/index-left.html
-
