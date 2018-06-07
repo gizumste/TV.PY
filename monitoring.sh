c05e4ec8d9c8685c039b4331cab1be39f4b634d9
gizumste <39944243+gizumste@users.noreply.github.com>
2018-06-07 09:45:34 +0200

    Create monitoring.sh

---
+++ monitoring.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+sleep 10s
+firefox-esr http://loxcom:mocxol@ene02-cam01.lextend.ch:20011/control/faststream.jpg?stream=$
+sleep 10s
+firefox-esr http://loxcom:mocxol@ene02-cam02.lextend.ch:20012/control/faststream.jpg?stream=$
+sleep 10s
+firefox-esr /home/pi/Documents/monitoring/index-left.html
+
